b118312205103b27b68ceaf587d95831627d099d
z41z <just4iu@sina.com>
2018-11-16 17:58:46 +0800

    Create upload.vue

---
+++ FrontEnd/upload.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-input type="file" ref="files" @change="getFiles"></el-input>
+    <el-button @click="upload">上传</el-button>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      currentFile: {}
+    };
+  },
+  methods: {
+    upload() {
+      let formData = new FormData()
+      formData.append('filename', this.currentFile)
+      axios.post('/api/upload.php', formData,
+        { headers: { 'Content-Type': 'multipart/form-data' } },
+      ).then(res => {
+        console.log(res)
+      })
+    },
+    getFiles() {
+      this.currentFile = event.currentTarget.files[0]
+    }
+  }
+}
+</script>
+<style>
+</style>
