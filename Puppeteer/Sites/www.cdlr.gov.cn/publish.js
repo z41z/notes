8b71583f3ca3db7880c74b4c75d0a5d5550110da
z41zP <isecfe@gmail.com>
2019-04-10 19:50:33 +0800

    add data

--- Puppeteer/Sites/www.cdlr.gov.cn/publish.js
+++ Puppeteer/Sites/www.cdlr.gov.cn/publish.js
@@ -5,26 +5,29 @@
 const puppeteer = require("puppeteer-core")
 const fs = require('fs');
 const request = require('request');
-let PAGE_TOTAL = 1;
+const chalk = require('chalk');
+let PAGE_NUM = 60;
 const URL = 'http://www.cdlr.gov.cn/second/zpgjg.aspx?ClassID=001002002006001';
 let result = []
-fs.readFile('./publish.json', function (err, res) {
-  result = !res.toString() ? [] : JSON.parse(res.toString())
-})
 
 const scrape = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
-    timeout: 50000,
+    // headless: false,
     executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
   })
 
   const page = await browser.newPage()
 
+  await page.setDefaultNavigationTimeout(120000)
   await page.goto(URL)
-  await page.waitForSelector('#AspNetPager1_input')
-  await page.select('#AspNetPager1_input', "" + 2);
   await page.waitFor(3000)
+  await page.evaluate((page) => {
+    __doPostBack('AspNetPager1', "" + page)
+  }, PAGE_NUM);
+  await page.waitForNavigation({
+    waitUntil: 'networkidle0'
+  })
+  await page.waitForSelector('#AspNetPager1_input')
   let subUrls = await page.evaluate(() => {
     let urls = []
     document.querySelectorAll('#sty > div.xxright > div > table > tbody > tr  > td> a').forEach(url => {
@@ -35,75 +38,66 @@ const scrape = async () => {
 
   for (let i = 0; i < subUrls.length; i++) {
     await page.goto(subUrls[i])
+    console.log(`${chalk.yellow(new Date)}`)
+    console.log(`链接【${chalk.blue(i+1)}】:${chalk.green(subUrls[i])}`)
     let infos = await page.evaluate(() => {
       let detail = [];
       let title = document.querySelector('#gtdetail > div > div.dlefttitle').innerText;
       let date = document.querySelector('#gtdetail > div > div.dleftinfo').innerText.split('|')[0].substring(3).trim();
 
-      document.querySelectorAll('#myFonts > div:nth-child(2) > table > tbody > tr').forEach((item, index) => {
-        if (index > 1) {
-          let code = item.querySelector('td:nth-child(2)').innerText || ''
-          let address = item.querySelector('td:nth-child(3)').innerText || ''
-          let area = item.querySelector('td:nth-child(4)').innerText || ''
-          let type = item.querySelector('td:nth-child(5)').innerText || ''
-          let startPrice = item.querySelector('td:nth-child(6)').innerText || ''
-          detail.push({
-            url: location.href,
-            title,
-            date,
-            code,
-            address,
-            area,
-            areaTrans: area.split('\n')[0],
-            type,
-            startPrice,
-            startPriceTrans: startPrice.indexOf('亩') > -1 ? (startPrice.replace(/[^(\d+\.?\d+)]/ig, '') / 0.0666).toFixed(2) : startPrice.replace(/[^(\d+\.?\d+)]/ig, '')
-          })
-        }
-      })
+      let infoEl = document.querySelectorAll('#myFonts table tbody tr');
+      if (infoEl.length) {
+        infoEl.forEach((item, index) => {
+          if (index > 1 && item.querySelector('td:nth-child(2)') && item.querySelector('td:nth-child(3)') && item.querySelector('td:nth-child(4)') && item.querySelector('td:nth-child(5)') && item.querySelector('td:nth-child(6)')) {
+            let code = item.querySelector('td:nth-child(2)').innerText || ''
+            let address = item.querySelector('td:nth-child(3)').innerText || ''
+            let area = item.querySelector('td:nth-child(4)').innerText || ''
+            let type = item.querySelector('td:nth-child(5)').innerText || ''
+            let startPrice = item.querySelector('td:nth-child(6)').innerText || ''
+            detail.push({
+              url: location.href,
+              title,
+              date,
+              code,
+              address,
+              area,
+              areaTrans: area.split('\n')[0],
+              type,
+              startPrice,
+              startPriceTrans: startPrice.indexOf('亩') > -1 ? (startPrice.replace(/[^(\d+\.?\d+)]/ig, '') / 0.0666).toFixed(2) : startPrice.replace(/[^(\d+\.?\d+)]/ig, '')
+            })
+          }
+        })
+      }
 
-      document.querySelectorAll('#myFonts > div:nth-child(2) img').forEach((img, index) => {
-        if (img && img.src.indexOf('upload') > -1) {
-          detail[index].img = img.src;
-        }
-      })
+      let imgsEl = document.querySelectorAll('#myFonts > div:nth-child(2) img')
+      if (imgsEl.length) {
+        imgsEl.forEach((img, index) => {
+          if (img && img.src.indexOf('upload') > -1 && detail[index]) {
+            detail[index].img = img.src;
+          }
+        })
+      }
 
       // detail.imgs = imgs;
       return detail;
 
     });
 
-    for (let i = 0; i < infos.length; i++) {
-      let img = infos[i].img;
-      infos[i].localImg = '';
-      if (img) {
-        let imgSplit = img.split('/');
-        infos[i].localImg = imgSplit[imgSplit.length - 1];
-        await request(img).pipe(fs.createWriteStream(`./images/${imgSplit[imgSplit.length - 1]}`));
+    if (infos.length) {
+      for (let i = 0; i < infos.length; i++) {
+        let img = infos[i].img || '';
+        infos[i].localImg = '';
+        if (img) {
+          let imgSplit = img.split('/');
+          infos[i].localImg = imgSplit[imgSplit.length - 1];
+          await request(img).pipe(fs.createWriteStream(`./images/${imgSplit[imgSplit.length - 1]}`));
+        }
       }
+      result = result.concat(infos)
+      fs.writeFile(`./data/publish${PAGE_NUM}.json`, JSON.stringify(result).replace(/\]\[/ig, ','), function (res) {})
     }
-    result = result.concat(infos)
-    fs.writeFile('publish.json', JSON.stringify(result).replace(/\]\[/ig, ','), function (res) { })
   }
-
-  if (PAGE_TOTAL > 1) {
-    PAGE_TOTAL--;
-    scrape()
-  }
-  // parseData();
-}
-const parseData = () => {
-  let content = [];
-  let codeArr = [];
-  fs.readFile('./publish.json', function (err, res) {
-    let data = (res.toString().replace(/\]\[/ig, ','))
-    JSON.parse(data).forEach(item => {
-      if (codeArr.indexOf(item.code) < 0) {
-        codeArr.push(item.code)
-        content.push(item)
-      }
-    })
-    fs.writeFile('./publish.json', JSON.stringify(content), function (err) { })
-  })
+  browser.close();
 }
 scrape();
--- Puppeteer/Sites/www.cdlr.gov.cn/result.js
+++ Puppeteer/Sites/www.cdlr.gov.cn/result.js
@@ -3,84 +3,101 @@
  */
 const puppeteer = require("puppeteer-core")
 const fs = require('fs');
-let PAGE_TOTAL = 1;
+const chalk = require('chalk');
+let PAGE_NUM = 60;
 const URL = 'http://www.cdlr.gov.cn/second/zpggg.aspx?ClassID=001002002006001';
-let result = [];
-fs.readFile('./result.json', function (err, res) {
-  result = !res.toString() ? [] : JSON.parse(res.toString())
-})
 
-const parseData = () => {
-  let content = [];
-  let codeArr = [];
-  fs.readFile('./result.json', function (err, res) {
-    let data = (res.toString().replace(/\]\[/ig, ','))
-    JSON.parse(data).forEach(item => {
-      if (codeArr.indexOf(item.code) < 0) {
-        codeArr.push(item.code)
-        content.push(item)
-      }
-    })
-    fs.writeFile('./result.json', JSON.stringify(content), function (err) { })
-  })
-}
 
 const scrape = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    // headless: false,
     timeout: 50000,
     executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
   })
 
   const page = await browser.newPage()
 
-  await page.goto(URL)
-  await page.waitForSelector('#AspNetPager1_input')
-  await page.select('#AspNetPager1_input', "" + 2);
-  await page.waitFor(3000)
-  let subUrls = await page.evaluate(() => {
-    let urls = []
-    document.querySelectorAll('#sty > div.xxright > div > table > tbody > tr  > td> a').forEach(url => {
-      urls.push(url.href)
+  await page.setDefaultNavigationTimeout(120000)
+  for (let m = 39; m < PAGE_NUM; m++) {
+    let result = [];
+    await page.goto(URL)
+    await page.waitFor(3000)
+    await page.evaluate((m) => {
+      __doPostBack('AspNetPager1', "" + m)
+    }, m);
+    await page.waitForNavigation({
+      waitUntil: 'networkidle0'
     })
-    return urls;
-  })
-
-  for (let i = 0; i < subUrls.length; i++) {
-    await page.goto(subUrls[i])
-    let infos = await page.evaluate(() => {
-      let detail = [];
-      document.querySelectorAll('#myFonts > div:nth-child(2) table  tbody tr').forEach((item, index) => {
-        if (index > 0) {
-          let code = item.querySelector('td:nth-child(2)').innerText || ''
-          let endPrice = item.querySelector('td:nth-child(5)').innerText == '/' ? '0' : item.querySelector('td:nth-child(5)').innerText
-          let endPriceTotal = item.querySelector('td:nth-child(6)').innerText == '/' ? '0' : item.querySelector('td:nth-child(6)').innerText
-          let owner = item.querySelector('td:nth-child(7)').innerText || ''
-          detail.push({
-            url: location.href,
-            code,
-            endPrice,
-            endPriceTrans: endPrice.indexOf('亩') > -1 ? (endPrice.replace(/[^(\d+\.?\d+)]/ig, '') / 0.0666).toFixed(2) : endPrice.replace(/[^(\d+\.?\d+)]/ig, ''),
-            endPriceTotal,
-            endPriceTotalTrans: endPriceTotal.replace(/[^(\d+\.?\d+)]/ig, ''),
-            owner
-          })
-        }
+    let subUrls = await page.evaluate(() => {
+      let urls = []
+      document.querySelectorAll('#sty > div.xxright > div > table > tbody > tr  > td> a').forEach(url => {
+        urls.push(url.href)
       })
+      return urls;
+    })
 
-      return detail;
+    for (let i = 0; i < subUrls.length; i++) {
+      await page.goto(subUrls[i])
+      console.log(`${chalk.yellow(new Date)}`)
+      console.log(`页${chalk.blue(m)}链接【${chalk.blue(i+1)}】:${chalk.green(subUrls[i])}`)
+      let infos = await page.evaluate(() => {
+        let detail = [];
+        document.querySelectorAll('#myFonts table  tbody tr').forEach((item, index) => {
+          if (index > 0) {
+            let code = ''
+            let endPrice = ''
+            let endPriceTotal = ''
+            let owner = ''
+            if (!item.querySelector('td:nth-child(7)')) {
+              if (item.querySelector('td:nth-child(6)')) {
+                if (item.querySelector('td:nth-child(6)').innerText == '&nbsp;') {
+                  code = item.querySelector('td:nth-child(1)').innerText || ''
+                  endPrice = item.querySelector('td:nth-child(4)').innerText == '/' ? '0' : item.querySelector('td:nth-child(4)').innerText
+                  endPriceTotal = item.querySelector('td:nth-child(4)').innerText == '/' ? '0' : item.querySelector('td:nth-child(4)').innerText
+                  owner = item.querySelector('td:nth-child(5)').innerText || ''
+                }
+                else{
+                  // http://www.cdlr.gov.cn/detailnoright.aspx?id=38579
+                  code = item.querySelector('td:nth-child(2)').innerText || ''
+                  endPrice = item.querySelector('td:nth-child(5)').innerText == '/' ? '0' : item.querySelector('td:nth-child(5)').innerText
+                  endPriceTotal = item.querySelector('td:nth-child(5)').innerText == '/' ? '0' : item.querySelector('td:nth-child(5)').innerText
+                  owner = item.querySelector('td:nth-child(6)').innerText || ''
+                }
+              } else {
+                // http://www.cdlr.gov.cn/detailnoright.aspx?id=32775
+                if(item.querySelector('td:nth-child(2)')){
+                  code = item.querySelector('td:nth-child(1)').innerText || ''
+                  endPrice = item.querySelector('td:nth-child(4)').innerText == '/' ? '0' : item.querySelector('td:nth-child(4)').innerText
+                  endPriceTotal = item.querySelector('td:nth-child(4)').innerText == '/' ? '0' : item.querySelector('td:nth-child(4)').innerText
+                  owner = item.querySelector('td:nth-child(5)').innerText || ''
+                }
+              }
+            } else {
+              code = item.querySelector('td:nth-child(2)').innerText || ''
+              endPrice = item.querySelector('td:nth-child(5)').innerText == '/' ? '0' : item.querySelector('td:nth-child(5)').innerText
+              endPriceTotal = item.querySelector('td:nth-child(6)').innerText == '/' ? '0' : item.querySelector('td:nth-child(6)').innerText
+              owner = item.querySelector('td:nth-child(7)').innerText || ''
+            }
+            detail.push({
+              url: location.href,
+              code,
+              endPrice,
+              endPriceTrans: endPrice.indexOf('亩') > -1 ? (endPrice.replace(/[^(\d+\.?\d+)]/ig, '') / 0.0666).toFixed(2) : endPrice.replace(/[^(\d+\.?\d+)]/ig, ''),
+              endPriceTotal,
+              endPriceTotalTrans: endPriceTotal.replace(/[^(\d+\.?\d+)]/ig, ''),
+              owner
+            })
+          }
+        })
 
-    });
+        return detail;
 
-    result = result.concat(infos)
-    fs.writeFile('./result.json', JSON.stringify(result), function (res) { })
+      });
+
+      result = result.concat(infos)
+      fs.writeFile(`./data/result${m}.json`, JSON.stringify(result), function (res) {})
+    }
   }
-
-  if (PAGE_TOTAL > 1) {
-    PAGE_TOTAL--;
-    scrape()
-  }
-  // parseData()
 }
 
 scrape();
